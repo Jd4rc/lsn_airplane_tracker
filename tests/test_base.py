4bd9949347c8f45a448726046cc91244e485645e
egor <e.nmitenin@gmail.com>
2026-07-16 10:58:14 +0300

    test: add tests for base API client

--- tests/test_base.py
+++ tests/test_base.py
@@ -2,6 +2,7 @@ import pytest
 
 from src.api.base import BaseAPIClient
 
+
 def test_api_base_cannot_be_initialized():
     with pytest.raises(TypeError):
         BaseAPIClient()
@@ -13,6 +14,7 @@ def test_child_without_get_data_cannot_be_initialized():
 
     with pytest.raises(TypeError):
         TestAPIWithoutGetData()
+
 
 def test_child_with_get_data_can_be_initialized():
     class TestAPIWithGetData(BaseAPIClient):
