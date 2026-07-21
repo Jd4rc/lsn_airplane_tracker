e59728359b636bd060f5842910d9af27fa2d487a
egor <e.nmitenin@gmail.com>
2026-07-21 10:05:33 +0300

    chore: fix linting issues

--- src/models/aircraft.py
+++ src/models/aircraft.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass(slots=True)
 class Aircraft:
     icao24: str
--- tests/test_aircraft.py
+++ tests/test_aircraft.py
@@ -1,29 +1,18 @@
 import pytest
 
-from models.aircraft import Aircraft
+from src.models.aircraft import Aircraft
+
 
 def test_from_opensky():
     # Arrange
-    state = [
-        'abc123',
-        '  124',
-        1591,
-        616161,
-        9891,
-        165.151115,
-        1415.116165,
-        1415.116165,
-        False,
-        65.197,
-        166.151165
-    ]
+    state = ["abc123", "  124", 1591, 616161, 9891, 165.151115, 1415.116165, 1415.116165, False, 65.197, 166.151165]
 
     # Act
     aircraft = Aircraft.from_opensky(state)
 
     # Assert
-    assert aircraft.icao24 == 'abc123'
-    assert aircraft.callsign == '124'
+    assert aircraft.icao24 == "abc123"
+    assert aircraft.callsign == "124"
     assert aircraft.longitude == 165.151115
     assert aircraft.latitude == 1415.116165
     assert aircraft.altitude == 1415.116165
@@ -33,23 +22,11 @@ def test_from_opensky():
 
 
 def test_from_opensky_with_none_callsign():
-    state = [
-        'abc123',
-        None,
-        1591,
-        616161,
-        9891,
-        165.151115,
-        1415.116165,
-        1415.116165,
-        False,
-        65.197,
-        166.151165
-    ]
+    state = ["abc123", None, 1591, 616161, 9891, 165.151115, 1415.116165, 1415.116165, False, 65.197, 166.151165]
 
     aircraft = Aircraft.from_opensky(state)
 
-    assert aircraft.icao24 == 'abc123'
+    assert aircraft.icao24 == "abc123"
     assert aircraft.callsign is None
     assert aircraft.longitude == 165.151115
     assert aircraft.latitude == 1415.116165
@@ -58,24 +35,13 @@ def test_from_opensky_with_none_callsign():
     assert aircraft.velocity == 65.197
     assert aircraft.heading == 166.151165
 
+
 def test_from_opensky_with_empty_callsign():
-    state = [
-        'abc123',
-        '',
-        1591,
-        616161,
-        9891,
-        165.151115,
-        1415.116165,
-        1415.116165,
-        False,
-        65.197,
-        166.151165
-    ]
+    state = ["abc123", "", 1591, 616161, 9891, 165.151115, 1415.116165, 1415.116165, False, 65.197, 166.151165]
 
     aircraft = Aircraft.from_opensky(state)
 
-    assert aircraft.icao24 == 'abc123'
+    assert aircraft.icao24 == "abc123"
     assert aircraft.callsign is None
     assert aircraft.longitude == 165.151115
     assert aircraft.latitude == 1415.116165
@@ -86,24 +52,12 @@ def test_from_opensky_with_empty_callsign():
 
 
 def test_from_opensky_with_none_optional_fields():
-    state = [
-        'abc123',
-        '  151gfg',
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        False,
-        None,
-        None
-    ]
+    state = ["abc123", "  151gfg", None, None, None, None, None, None, False, None, None]
 
     aircraft = Aircraft.from_opensky(state)
 
-    assert aircraft.icao24 == 'abc123'
-    assert aircraft.callsign == '151gfg'
+    assert aircraft.icao24 == "abc123"
+    assert aircraft.callsign == "151gfg"
     assert aircraft.longitude is None
     assert aircraft.latitude is None
     assert aircraft.altitude is None
@@ -116,5 +70,3 @@ def test_from_opensky_with_empty_state():
     state = []
     with pytest.raises(IndexError):
         Aircraft.from_opensky(state)
-
-
