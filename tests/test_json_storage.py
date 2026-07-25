3f780a79d9207d97ea0b9fdfe251aaa45248c266
dosed <e.nmitenin@gmail.com>
2026-07-25 09:22:12 +0300

    test: add tests for top aircraft by altitude

--- tests/test_json_storage.py
+++ tests/test_json_storage.py
@@ -1,6 +1,5 @@
 import json
 
-from src.models import aircraft
 from src.models.aircraft import Aircraft
 from src.storage.json_storage import JsonStorage
 
@@ -21,16 +20,14 @@ def test_save_creates_json_storage(tmp_path):
         )
     ]
 
-    storage.save(
-        aircraft=aircraft,
-        location='Moscow'
-    )
+    storage.save(aircraft=aircraft, location="Moscow")
 
-    files = list(tmp_path.glob('moscow_*.json'))
+    files = list(tmp_path.glob("moscow_*.json"))
 
     assert len(files) == 1
 
-def test_save_creates_json_storage(tmp_path):
+
+def test_save_creates_correct_json_data(tmp_path):
     storage = JsonStorage(data_directory=tmp_path)
 
     aircraft = [
@@ -46,30 +43,27 @@ def test_save_creates_json_storage(tmp_path):
         )
     ]
 
-    storage.save(
-        aircraft=aircraft,
-        location='Moscow'
-    )
+    storage.save(aircraft=aircraft, location="Moscow")
 
-    saved_file = next(tmp_path.glob('moscow_*.json'))
+    saved_file = next(tmp_path.glob("moscow_*.json"))
 
-    with open(saved_file, encoding='utf-8') as f:
+    with open(saved_file, encoding="utf-8") as f:
         saved_data = json.load(f)
 
     assert saved_data == {
         "location": "Moscow",
         "aircraft": [
             {
-                'icao24': '152022',
-                'callsign': 'AFL1544',
-                'latitude': 55.75,
-                'longitude': 37.61,
-                'altitude': 4503.42,
-                'velocity': 27.57,
-                'heading': 75.96,
-                'on_ground': False,
+                "icao24": "152022",
+                "callsign": "AFL1544",
+                "latitude": 55.75,
+                "longitude": 37.61,
+                "altitude": 4503.42,
+                "velocity": 27.57,
+                "heading": 75.96,
+                "on_ground": False,
             }
-        ]
+        ],
     }
 
 
@@ -96,19 +90,16 @@ def test_save_writes_all_aircraft(tmp_path):
             velocity=27.99,
             heading=162.9,
             on_ground=False,
-        )
+        ),
     ]
 
-    storage.save(
-        aircraft=aircraft,
-        location='Moscow'
-    )
+    storage.save(aircraft=aircraft, location="Moscow")
 
-    file = next(tmp_path.glob('moscow_*.json'))
+    file = next(tmp_path.glob("moscow_*.json"))
 
-    with file.open(encoding='utf-8') as f:
+    with file.open(encoding="utf-8") as f:
         saved_data = json.load(f)
 
-    assert len(saved_data['aircraft']) == 2
-    assert saved_data['aircraft'][0]['icao24'] == '152022'
-    assert saved_data['aircraft'][1]['icao24'] == '151d8b'
+    assert len(saved_data["aircraft"]) == 2
+    assert saved_data["aircraft"][0]["icao24"] == "152022"
+    assert saved_data["aircraft"][1]["icao24"] == "151d8b"
