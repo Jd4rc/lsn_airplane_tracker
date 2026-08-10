5e751a54817afb55e809daebca5024112b025244
dosed <e.nmitenin@gmail.com>
2026-08-10 09:13:06 +0300

    feat: add aircraft count method to DBManager

--- src/database/db_manager.py
+++ src/database/db_manager.py
@@ -3,4 +3,19 @@ from src.database.connection import get_connection
 
 class DBManager:
     def get_countries_and_aeroplanes_count(self) -> list[tuple[str, int]]:
-        pass
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    SELECT
+                        countries.name,
+                        COUNT(aeroplanes.id)
+                    FROM countries
+                    LEFT JOIN aeroplanes
+                        ON countries.id = aeroplanes.country_id
+                    GROUP BY countries.id, countries.name
+                    ORDER BY countries.name;
+                    """
+                )
+
+                return cur.fetchall()
